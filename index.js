9471ecef22a21333388d68611e032ba4c06fbfe6
wooorm <tituswormer@gmail.com>
2014-07-19 18:57:01 +0200

    Added simple website

---
+++ index.js
@@ -0,0 +1,40 @@
+var franc = require('franc');
+
+var inputElement = document.getElementsByTagName('textarea')[0];
+var outputElement = document.getElementsByTagName('ol')[0];
+var buttonElement = document.getElementsByTagName('button')[0];
+var wrapperElement = document.getElementsByTagName('div')[0];
+
+buttonElement.addEventListener('click', detectLanguage);
+
+wrapperElement.style.display = 'none';
+
+function detectLanguage() {
+    visualiseResults(franc.all(inputElement.textContent));
+}
+
+function visualiseResults(results) {
+    wrapperElement.style.display = '';
+    cleanOutputElement();
+    results = results.map(createResult);
+    
+    results.forEach(function (node) {
+        outputElement.appendChild(node);
+    });
+
+    results[0].classList.add('franc__probable')
+}
+
+function cleanOutputElement() {
+    while (outputElement.firstElementChild) {
+        outputElement.removeChild(outputElement.firstElementChild);
+    }
+}
+
+function createResult(result, n) {
+    var node = document.createElement('li');
+
+    node.textContent = result[0] + ': ' + result[1];
+
+    return node;
+}
