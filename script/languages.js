7a4efa21e59f3fa288f977af3a7ebfc412ce1185
wooorm <tituswormer@gmail.com>
2014-09-30 12:27:56 +0200

    Add ISO-639-3 names file, remove ISO-639-2 names file

---
+++ script/languages.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * Dependencies.
+ */
+
+var langs;
+
+langs = require('langs');
+
+/**
+ * Data.
+ */
+
+var data;
+
+data = {};
+
+langs.all().forEach(function (language) {
+    data[language[3]] = language.name;
+})
+
+data.arb = 'Arabic, Standard';
+data.azj = 'Azerbaijani, North';
+data.ceb = 'Cebuano';
+data.pes = 'Persian, Iranian';
+data.haw = 'Hawaiian';
+data.khk = 'Mongolian, Halh';
+data.nso = 'Sotho, Northern';
+data.por = 'Portuguese (European)';
+data.als = 'Albanian, Tosk';
+data.swh = 'Swahili';
+data.uzn = 'Uzbek, Northern';
+data.cmn = 'Chinese, Mandarin';
+data.und = 'Undetermined';
+
+/**
+ * Expose `data`.
+ */
+
+module.exports = data;
